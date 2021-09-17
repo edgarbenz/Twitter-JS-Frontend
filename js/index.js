f49dc4e5c6f1afa47b79bf4ed0cbb5c1dc7a864a
Edgar Benzor <edgarbenz@hotmail.com>
2021-09-17 11:45:50 -0500

    meti todo el codigo en un funcion llamada startup

--- js/index.js
+++ js/index.js
@@ -1,37 +1,38 @@
+function startup() {
+    const loader = document.querySelector(".lds-facebook");
+    loader.classList.add("hidden");
 
+    const postList = document.querySelector(".posts-list");
 
+    const tweets = [
+        {
+            author: "@edgar",
+            message: "Lorem fistrum quietooor diodenoo sed duis benemeritaar velit qué dise usteer",
+            date: "2021-09-17 09:07:00"
 
-const loader = document.querySelector(".lds-facebook");
-loader.classList.add("hidden");
+        },
+        {
+            author: "@pacheco",
+            message: "Dolore consequat commodo ullamco la caidita me cago en tus muelas está la cosa",
+            date: "2021-09-17 09:12:00"
+        },
+        {
+            author: "@rou",
+            message: "Al ataquerl ahorarr a peich benemeritaar va usté muy cargadoo amatomaa la caidita",
+            date: "2021-09-17 10:57:00"
+        }
+    ];
 
-const postList = document.querySelector(".posts-list");
+    tweets.forEach(tweet => {
+        const tweetElement =  document.createElement("article");
+        const tweetHTML = `<div class="posts">
+                            <strong class= "author">${tweet.author}></strong>
+                            <p class= "message">${tweet.message}</p>
+                            <time datetime="${tweet.date}">${tweet.date}</time>
+                        </div>`;
+        tweetElement.innerHTML = tweetHTML;
+        postList.appendChild(tweetElement);
+    })
 
-const tweets = [
-    {
-        author: "@edgar",
-        message: "Lorem fistrum quietooor diodenoo sed duis benemeritaar velit qué dise usteer",
-        date: "2021-09-17 09:07:00"
-
-    },
-    {
-        author: "@pacheco",
-        message: "Dolore consequat commodo ullamco la caidita me cago en tus muelas está la cosa",
-        date: "2021-09-17 09:12:00"
-    },
-    {
-        author: "@rou",
-        message: "Al ataquerl ahorarr a peich benemeritaar va usté muy cargadoo amatomaa la caidita",
-        date: "2021-09-17 10:57:00"
-    }
-];
-
-tweets.forEach(tweet => {
-    const tweetElement =  document.createElement("article");
-    const tweetHTML = `<div class="posts">
-                          <strong class= "author">${tweet.author}></strong>
-                          <p class= "message">${tweet.message}</p>
-                          <time datetime="${tweet.date}">${tweet.date}</time>
-                       </div>`;
-    tweetElement.innerHTML = tweetHTML;
-    postList.appendChild(tweetElement);
-})
+}
+window.addEventListener("DOMContentLoaded", startup);
